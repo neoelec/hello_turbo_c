bbc45c268bb6cd1989b79531b4409896589455c1
YOUNGJIN JOO <neoelec@gmail.com>
2025-12-28 21:45:34 +0900

    ex11 - updated
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- ex11_5/ex11_5.c
+++ ex11_5/ex11_5.c
@@ -1,4 +1,3 @@
-#include <io.h>
 #if !defined(__TINY__) && !defined(__SMALL__) && !defined(__MEDIUM__)
 #error Use tiny, small or medium memory model.
 #endif
@@ -20,32 +19,29 @@ int main(int argc, char *argv[])
     if (argc != 3) {
         printf("USAGE : ex11_5 filename1 filename2\n");
 
-        return 0;
+        return -1;
     }
 
     if ((src = open(argv[1], O_RDONLY | O_BINARY)) == -1) {
         printf("File open error : %s\n", argv[1]);
 
-        return 0;
+        return -1;
     }
 
-    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_BINARY,
-                S_IREAD | S_IWRITE);
-
-    if (dest == -1) {
+    if ((dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_BINARY,
+                     S_IREAD | S_IWRITE)) == -1) {
         printf("File creation error : %s\n", argv[2]);
 
-        return 0;
+        return -1;
     }
 
     printf("Copy %s to %s\n", argv[1], argv[2]);
     memfree = coreleft();
-    buf = malloc(memfree);
 
-    if (buf == NULL) {
+    if ((buf = malloc(memfree)) == NULL) {
         printf("Memory allocation error\n");
 
-        return 0;
+        return -1;
     }
 
     filesize = filelength(src);
