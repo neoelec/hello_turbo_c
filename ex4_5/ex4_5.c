7725854b575142cc2c90b21e3ba2e1f382edf52e
YOUNGJIN JOO <neoelec@gmail.com>
2026-01-18 12:57:25 +0900

    ex4_5: turbo-c doex not support %zu format specifier
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- ex4_5/ex4_5.c
+++ ex4_5/ex4_5.c
@@ -6,16 +6,16 @@ int main(void)
     char b[40];
 
     printf("The size of some fundamental types is computed.\n\n");
-    printf("       char:%3zu byte\n", sizeof(char));
-    printf("      short:%3zu bytes\n", sizeof(short));
-    printf("        int:%3zu bytes\n", sizeof(int));
-    printf("       long:%3zu bytes\n", sizeof(long));
-    printf("   unsigned:%3zu bytes\n", sizeof(unsigned));
-    printf("      float:%3zu bytes\n", sizeof(float));
-    printf("     double:%3zu bytes\n", sizeof(double));
-    printf("long double:%3zu bytes\n", sizeof(long double));
-    printf("The variable a has %3zu bytes.\n", sizeof(a));
-    printf("The variable b has %3zu bytes.\n", sizeof(b));
+    printf("       char:%3u byte\n", sizeof(char));
+    printf("      short:%3u bytes\n", sizeof(short));
+    printf("        int:%3u bytes\n", sizeof(int));
+    printf("       long:%3u bytes\n", sizeof(long));
+    printf("   unsigned:%3u bytes\n", sizeof(unsigned));
+    printf("      float:%3u bytes\n", sizeof(float));
+    printf("     double:%3u bytes\n", sizeof(double));
+    printf("long double:%3u bytes\n", sizeof(long double));
+    printf("The variable a has %3u bytes.\n", sizeof(a));
+    printf("The variable b has %3u bytes.\n", sizeof(b));
 
     return 0;
 }
